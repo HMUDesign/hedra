86167553dec073924388c798d8edc89fe1fcdb4c
Christopher Baker <christopher@hmudesign.com>
2015-06-13 15:30:15 -0700

    feature complete

--- app.js
+++ app.js
@@ -1,21 +1,97 @@
 import Debug from 'debug';
 let debug = Debug('hedra:app');
 
-import Emitter from './lib/emitter';
 import Hedra from './hedra';
-
 import THREE from 'vendor/three';
 
-export default class App extends Emitter {
+export default class App extends Hedra {
 	constructor(config) {
-		super();
-	}
-	
-	add(child) {
-		if(!child instanceof Hedra) {
-			throw new Error('Hedra.app.add: Child not an instance of `Hedra`.');
+		config.make = false;
+		config.parent = config.parent || document.body;
+		
+		if(!config.width && !config.height) {
+			config.width = window.innerWidth;
+			config.height = window.innerHeight;
+			
 		}
 		
-		console.log(child)
+		if(config.parent === document.body) {
+			document.body.style.margin = '0';
+		}
+		
+		var scene = new THREE.Scene();
+		
+		var camera = new THREE.PerspectiveCamera(75, config.width / config.height, 0.1, 1000);
+		camera.position.copy(config.camera || new THREE.Vector3(0,0,0));
+		
+		var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+		renderer.setClearColor(0x000000, 0);
+		renderer.setSize(config.width, config.height);
+		
+		renderer.domElement.style.display = 'block';
+		config.parent.appendChild(renderer.domElement);
+		
+		super(config);
+		
+		this.scene = this._ = scene;
+		this.camera = camera;
+		this.renderer = renderer;
+		
+		this.scene.add(this.camera);
+		
+		this.clock = new THREE.Clock(false);
+		
+		if(config.pause) {
+			window.addEventListener('focus', () => { this.play(); }, false);
+			window.addEventListener('blur', () => { this.pause(); }, false);
+		}
+		
+		setTimeout(() => { this.play(); }, 0);
+		
+		
+		if(typeof Stats !== 'undefined') {
+			this.stats = new Stats();
+			
+			this.stats.domElement.style.position = 'absolute';
+			this.stats.domElement.style.top  = '0';
+			this.stats.domElement.style.left = '0';
+			config.parent.appendChild(this.stats.domElement);
+		}
+		
+		if(typeof TWEEN !== 'undefined') {
+			TWEEN._time = 0;
+			this.on('update').then((e) => {
+				TWEEN._time = e.time;
+				TWEEN.update(e.time);
+			});
+		}
+	}
+	
+	play() {
+		this.clock.start();
+		requestAnimationFrame(() => { this.animate(); });
+	}
+	
+	pause() {
+		this.clock.stop();
+	}
+	
+	animate() {
+		if(this.clock.running) {
+			requestAnimationFrame(() => { this.animate(); });
+		}
+		
+		this.frame(this.clock.getDelta(), this.clock.getElapsedTime());
+	}
+	
+	frame(delta, time) {
+		this.bubble('update', delta || 0, time || 0);
+		
+		this.render();
+		if(this.stats) this.stats.update();
+	}
+	
+	render() {
+		this.renderer.render(this.scene, this.camera);
 	}
 }
--- hedra.js
+++ hedra.js
@@ -6,15 +6,72 @@ import THREE from 'vendor/three';
 
 export default class Hedra extends Emitter {
 	constructor(config) {
-		super();
-		
-		if(config.make !== false) {
+		if(config.make === false) {
+			delete config.make;
+		}
+		else {
 			if(config.geometry && config.material) {
-				this._ = new THREE.Mesh(config.geometry, config.material)
+				config.make = new THREE.Mesh(config.geometry, config.material);
 			}
 			else {
-				this._ = config.make || new THREE.Object3D();
+				config.make = new THREE.Object3D();
 			}
 		}
+		
+		super();
+		
+		this.children = [];
+		
+		if(config.make) {
+			this._ = config.make;
+			this._._ = this;
+		}
+	}
+	
+	add(child) {
+		if(!child instanceof Hedra) {
+			throw new Error('Hedra.app.add: Child not an instance of `Hedra`.');
+		}
+		
+		this.children.push(child);
+		this._.add(child._);
+	}
+	
+	bubble(...items) {
+		this.emit(...items);
+		
+		for(let child of this.children) {
+			child.bubble(...items);
+		}
+	}
+	
+	tween(config) {
+		var tween = new TWEEN.Tween(config.object || this);
+		
+		if(config.to && config.duration) {
+			tween.to(config.to, config.duration);
+		}
+		
+		if(config.easing) {
+			tween.easing(config.easing);
+		}
+		
+		var promise = new Promise(function(resolve, reject) {
+			tween.onComplete(resolve);
+			tween.onStop(resolve);
+			tween.start(TWEEN._time);
+		});
+		
+		promise.tween = tween;
+		
+		return promise;
+	}
+	
+	get position() {
+		return this._.position;
+	}
+	
+	get rotation() {
+		return this._.rotation;
 	}
 }
