79e4a20454143d3f31870991571958c0dfcbdab3
Christopher Baker <christopher@bitovi.com>
2018-11-26 14:39:00 -0800

    clean old

--- index.js
+++
@@ -1,9 +0,0 @@
-import App from './app';
-import Component from './component';
-import * as plugins from './plugins';
-
-export {
-	App,
-	Component,
-	plugins,
-};
--- plugins/index.js
+++
@@ -1,9 +0,0 @@
-import keys from './keys';
-import mouse from './mouse';
-import resize from './resize';
-
-export {
-	keys,
-	mouse,
-	resize,
-};
